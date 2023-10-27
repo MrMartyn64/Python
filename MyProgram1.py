3330a395f1a3b535fe5873dd3dfb760122620c30
MrMartyn64 <149187647+MrMartyn64@users.noreply.github.com>
2023-10-27 12:27:07 -0400

    Add cool shee

--- MyProgram1.py
+++ MyProgram1.py
@@ -5,3 +5,6 @@ x = input("how much?")
 print ("It costs: "+x)
 print ("Happy Halloween!")
 print ("Happy Halloween!")
+myList = {"ab", 34, True, 40, "male"}
+print("THe lenght of thisList:", len(myList))
+print(myList)
